1ec424e2cbfaf156758127dd76cbeea770ee7ac7
Michael9921 <michael.dharmawan1@student.umn.ac.id>
2023-03-03 16:05:44 +0700

    Comparing abstract classes and interface

---
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -0,0 +1,59 @@
+package example.myapp
+
+//interface FishAction {
+//    fun eat()
+//}
+//abstract class AquariumFish {
+//    abstract val color: String
+//    override fun eat() = println("yum")
+//}
+//class Shark: AquariumFish() {
+//    override val color = "grey"
+//    override fun eat() {
+//        println("hunt and eat fish")
+//    }
+//}
+//class Plecostomus: AquariumFish() {
+//    override val color = "gold"
+//    override fun eat() {
+//        println("eat algae")
+//    }
+
+    interface FishAction {
+        fun eat()
+    }
+    interface FishColor {
+        val color: String
+    }
+    class Plecostomus: FishAction, FishColor {
+        override val color = "gold"
+        override fun eat() {
+            println("eat algae")
+        }
+    }
+    class Shark: FishAction, FishColor {
+        override val color = "grey"
+        override fun eat() {
+            println("hunt and eat fish")
+        }
+    }
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+class Plecostomus(fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+//    FishColor by fishColor {
+//    override fun eat() {
+//        println("eat algae")
+//    }
+//}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
+
+
--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,11 +1,11 @@
 package example.myapp
 
-fun buildAquarium() {
+//fun buildAquarium() {
 //    val myAquarium = Aquarium()
 //    myAquarium.printSize()
 //    myAquarium.height = 60
 //    myAquarium.printSize()
-
+//
 //    val aquarium1 = Aquarium()
 //    aquarium1.printSize()
 //    // default height and length
@@ -23,17 +23,29 @@ fun buildAquarium() {
 //    aquarium6.printSize()
 //    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
 //    aquarium6.printSize()
-    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
-    myAquarium.printSize()
-    val myTower = TowerTank(diameter = 25, height = 40)
-    myTower.printSize()
+//    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+//    myAquarium.printSize()
+//    val myTower = TowerTank(diameter = 25, height = 40)
+//    myTower.printSize()
+//
+//}
+//fun main() {
+//    buildAquarium()
+//}
+//
+//class Main {
+//}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+
 
 }
-
-
-fun main() {
-    buildAquarium()
-}
-
-class Main {
+fun main () {
+    makeFish()
 }
